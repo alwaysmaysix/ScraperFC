68711a0edd41fc4b1547970da34ab2f67c9ae823
alwaysmaysix <104639059+alwaysmaysix@users.noreply.github.com>
2024-08-08 02:10:31 +0200

    Update clubelo.py

--- src/ScraperFC/clubelo.py
+++ src/ScraperFC/clubelo.py
@@ -1,11 +1,54 @@
+import google_colab_selenium as gs
+from selenium.webdriver.chrome.options import Options
+from random import choice
 from datetime import datetime
 from io import StringIO
 import pandas as pd
-import requests
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
 from .scraperfc_exceptions import ClubEloInvalidTeamException
 
+import time
+
+user_agents = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; AS; rv:11.0) like Gecko',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.1 Safari/605.1.15',
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.96 Safari/537.36'
+]
+
+def setup_selenium():
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--disable-infobars")
+    options.add_argument("--disable-popup-blocking")
+    options.add_argument("--ignore-certificate-errors")
+    options.add_argument("--incognito")
+    options.add_argument(f'--user-agent={choice(user_agents)}')
+    driver = gs.Chrome(options=options)
+    return driver
+
+def get_page_content(url, driver, request_interval=2, page_load_delay=2):
+    driver.get(url)
+    time.sleep(request_interval)
+    html_content = driver.page_source
+    time.sleep(page_load_delay)
+    return html_content
 
 class ClubElo:
+
+    def __init__(self):
+        self.driver = setup_selenium()
+
+    def __del__(self):
+        self.driver.close()
+        self.driver.quit()
 
     # ==============================================================================================
     def scrape_team_on_date(self, team: str, date: str) -> float:
@@ -30,15 +73,11 @@ class ClubElo:
         if not isinstance(date, str):
             raise TypeError('`date` must be a string.')
 
-        # Use ClubElo API to get team data as Pandas DataFrame
+        # Use Selenium to get team data as Pandas DataFrame
         url = f'http://api.clubelo.com/{team}'
-        while 1:
-            try:
-                r = requests.get(url)
-                break
-            except requests.exceptions.ConnectionError:
-                pass
-        df = pd.read_csv(StringIO(r.text), sep=',')
+        page_content = get_page_content(url, self.driver)
+        
+        df = pd.read_csv(StringIO(page_content), sep=',')
         if df.shape[0] == 0:
             raise ClubEloInvalidTeamException(team)
 
